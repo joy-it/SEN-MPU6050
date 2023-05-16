ffd51857b9cf6c6ea7bafaa4d1e32ae3335e9329
MaiderBur <49188547+MaiderBur@users.noreply.github.com>
2023-05-16 13:45:56 +0200

    modified:   SEN-MPU6050.ts

--- SEN-MPU6050.ts
+++ SEN-MPU6050.ts
@@ -345,7 +345,7 @@ namespace SENMPU6050 {
             q[1] = q[1] * recipNorm;
             q[2] = q[2] * recipNorm;
             q[3] = q[3] * recipNorm;
-            serial.writeValue("q", q))
+            serial.writeValue("q", q);
 
             return q;
             
